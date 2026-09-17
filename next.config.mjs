7a8059e80c11c27ef6355a26d3978a4f9fbab2e9
thomas-savigner <tomdevtools@gmail.com>
2026-09-17 16:53:02 +0200

    set static export building app

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: [
     'local-origin.dev', '*.local-origin.dev', '192.168.1.30'
   ],
