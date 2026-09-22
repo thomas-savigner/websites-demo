3337f42e50a25692bbf4fa3f87410dd1e1143f60
thomas-savigner <tomdevtools@gmail.com>
2026-09-22 11:59:27 +0200

    feat: configurer le chemin de base pour GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
+const repo = websites-demo;
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isGithubPages ? `/${repo}` : '',
+  trailingSlash: true,
   allowedDevOrigins: [
     'local-origin.dev', '*.local-origin.dev', '192.168.1.30'
   ],
