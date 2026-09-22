0b31236f10a4c7c2ab981142b11d8ba35ac54f9c
thomas-savigner <tomdevtools@gmail.com>
2026-09-22 12:19:10 +0200

    feat: ajouter la fonction getAssetPath et améliorer la configuration de base dans next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,30 @@
 /** @type {import('next').NextConfig} */
+
 const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
 
-const repo = "websites-demo";
+const repo = 'websites-demo';
+
+const basePath = isGithubPages ? `/${repo}` : '';
 
 const nextConfig = {
   output: 'export',
+
   images: {
     unoptimized: true,
   },
-  basePath: isGithubPages ? `/${repo}` : '',
+
+  basePath,
+
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
+
   trailingSlash: true,
+
   allowedDevOrigins: [
-    'local-origin.dev', '*.local-origin.dev', '192.168.1.30'
+    'local-origin.dev',
+    '*.local-origin.dev',
+    '192.168.1.30',
   ],
 };
 
